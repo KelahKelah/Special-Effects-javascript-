779ed375cef34bde9a5765094d77dd112ef42ac3
KelahKelah <kelechiekebor@gmail.com>
2020-09-18 17:11:56 +0100

    toggle between components at app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/home/home';
 import Header from './components/header/header';
+import ContactUs from './pages/contactUs/contactUs';
 import Footer from './components/footer/footer';
+import AboutUs from './pages/aboutUs/aboutUs';
 
 function App() {
   return (
     <div>
       <Header />
-      <Home />
+      {/* <Home /> */}
+      <ContactUs />
+      {/* <AboutUs /> */}
       <Footer />
     </div>
   );
 }
 
 export default App;
+
