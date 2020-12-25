ba846a6c5db0f9a433062eda00c115f6ed0c2d79
KelahKelah <kelechiekebor@gmail.com>
2020-12-25 02:39:10 +0100

    design products page and add more style to about us and contact page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/header/header';
 import ContactUs from './pages/contactUs/contactUs';
 import Footer from './components/footer/footer';
 import AboutUs from './pages/aboutUs/aboutUs';
+import Products from './pages/products/products';
 import {Switch, BrowserRouter, Route, Link} from 'react-router-dom';
 
 function App() {
@@ -13,13 +14,13 @@ function App() {
       <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
         <Route path="/about" component={AboutUs} />
         <Route path="/contact" component={ContactUs} />
+        <Route path="/products" component={Products} />
+
       </Switch>
       </BrowserRouter>
-      {/* <Home /> */}
-      {/* <ContactUs /> */}
-      {/* <AboutUs /> */}
       <Footer />
     </div>
   );
